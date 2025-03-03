ed3085db1b5c33aa6a6fba573f7ecc901b2ba29a
Luanf38 <luanfelipe38@hotmail.com>
2025-03-03 19:46:44 -0300

    adicionado Shape personalizado no materialTheme

--- app/src/main/java/com/luanlima/superheroapp/ui/theme/Theme.kt
+++ app/src/main/java/com/luanlima/superheroapp/ui/theme/Theme.kt
@@ -108,6 +108,7 @@ fun SuperHeroAppTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
